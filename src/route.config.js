65dd4d43ac9fbc9f2644c8fc49deb9b35259927f
feizheng <1290657123@qq.com>
2016-10-08 13:38:09 +0800

    update mui styles

--- src/route.config.js
+++ src/route.config.js
@@ -3,9 +3,6 @@ export default function(router) {
     '/': {
       component: require('./views/index/index.vue')
     },
-    '/button': {
-      component: require('./views/button/index.vue')
-    },
     '*': {
       component: require('./views/404/index.vue')
     }
